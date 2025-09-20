af9c21220e76748b45270354506160ee708b7a30
Yevrews Swervey <keeganlenz12@gmail.com>
2025-09-20 16:30:09 -0600

    setting up landing page

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import LandingPage from './landing_page.tsx'
+
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <div className="page_wrapper">
+            <LandingPage></LandingPage>
+        </div>
+    </StrictMode>,
+);
---
+++ src/landing_page.tsx
@@ -0,0 +1,33 @@
+import './landing_page.scss'
+import profile_pic from './assets/landing_page/profile_pic.jpg'
+
+function LandingPage() {
+    return (
+        <div className="landing_page">
+            <ProfilePicture></ProfilePicture>
+            <Introduction></Introduction>
+        </div>
+    );
+}
+
+function ProfilePicture() {
+    return (
+        <div className="profile_picture_wrapper">
+            <img src={profile_pic} alt="Picture of Keegan Lenz"></img>
+        </div>
+    );
+}
+
+function Introduction() {
+    return (
+        <div className="introduction">
+            <header>
+                <h1>Hi, I'm <span style={{ color: "#8d697a" }}>Keegan.</span></h1>
+                <h2>A software developer</h2>
+            </header>
+            <p>Multidisciplinary programmer based in Salt Lake City. Experienced problem-solver, eager to learn and build. I develop full-stack web applications, video games, and graphics programs.</p>
+        </div>
+    );
+}
+
+export default LandingPage
