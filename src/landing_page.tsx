df1a9679684ad2e9cd92cf1a3b6180eb2e3401ac
Yevrews Swervey <keeganlenz12@gmail.com>
2025-09-25 20:43:42 -0600

    initial contact bar

--- src/landing_page.tsx
+++ src/landing_page.tsx
@@ -11,14 +11,15 @@ import profile_pic from './assets/landing_page/images/profile_pic.jpg'
 function LandingPage() {
     return (
         <div className="landing_page">
-            <BackgroundImages></BackgroundImages>
+            <LandingImages></LandingImages>
             <ProfilePicture></ProfilePicture>
+            <ContactLinks></ContactLinks>
             <Introduction></Introduction>
         </div>
     );
 }
 
-function BackgroundImages() {
+function LandingImages() {
     return (
         <div className="background_images_wrapper">
             <img className="journal" src={journal} alt="Picture of journal"></img>
@@ -36,6 +37,28 @@ function ProfilePicture() {
             <img src={profile_pic} alt="Picture of Keegan Lenz"></img>
         </div>
     );
+}
+
+function ContactLinks() {
+    return (
+        <div className="contact_links_wrapper">
+            <button className="image_button" type="button">
+               <i className="fa">&#xf09b; </i>
+            </button>
+
+            <button className="image_button" type="button">
+                <i className="fa">&#xf08c;</i>
+            </button>
+
+            <button className="image_button" type="button">
+                &#x2709;
+            </button>
+
+            <button className="call_to_action" type="button">
+                Explore More
+            </button>
+        </div>
+    )
 }
 
 function Introduction() {
