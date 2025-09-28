5dafdb78893ed803ca8cfa2d3b44ee59215220df
Yevrews Swervey <keeganlenz12@gmail.com>
2025-09-28 09:30:45 -0600

    finished landing page mockup

--- src/landing_page.tsx
+++ src/landing_page.tsx
@@ -42,19 +42,19 @@ function ProfilePicture() {
 function ContactLinks() {
     return (
         <div className="contact_links_wrapper">
-            <button className="image_button" type="button">
+            <button className="image_button">
                <i className="fa">&#xf09b; </i>
             </button>
 
-            <button className="image_button" type="button">
+            <button className="image_button">
                 <i className="fa">&#xf08c;</i>
             </button>
 
-            <button className="image_button" type="button">
-                &#x2709;
+            <button className="image_button" id="mail">
+                <i className="far">&#xf0e0;</i>
             </button>
 
-            <button className="call_to_action" type="button">
+            <button className="call_to_action">
                 Explore More
             </button>
         </div>
@@ -65,7 +65,7 @@ function Introduction() {
     return (
         <div className="introduction">
             <header>
-                <h1>Hi, I'm <span style={{ color: "#8d697a" }}>Keegan.</span> A software developer.</h1>
+                <h1>Hi, I'm <span style={{ color: "#4444c5" }}>Keegan.</span> A software developer.</h1>
             </header>
             <p>Multidisciplinary programmer based in Salt Lake City. Experienced problem-solver, eager to learn and build. I develop full-stack web applications, video games, and graphics programs.</p>
         </div>
